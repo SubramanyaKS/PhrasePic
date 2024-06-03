195d2138b7658c1b2f2342e96e6c01ec72c082c8
Subramanya K S <subramanyaks2205@gmail.com>
2024-06-03 23:14:46 +0530

    Added footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='font-semibold text-pink-400'>Developed by Subramanya, Supported by Hugging Face</footer>
+  )
+}
+export default Footer
