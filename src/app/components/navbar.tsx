60c41a321c3ae5509e5306dcaaf5005f53b7ab2f
Subramanya K S <subramanyaks2205@gmail.com>
2024-06-05 23:11:23 +0530

    included aria-label

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
         </div>
         <div className="md:hidden">
-          <button onClick={toggleMenu} className="text-gray-300 hover:text-white focus:outline-none">
+          <button onClick={toggleMenu} aria-label="menu" className="text-gray-300 hover:text-white focus:outline-none">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               {isOpen ? (
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
