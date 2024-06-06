b1c6b1dde1e5852f5a35e3a80cbbfecec4acaa8f
Subramanya K S <subramanyaks2205@gmail.com>
2024-06-06 21:56:06 +0530

    added name prop

--- src/app/components/inputfeild.tsx
+++ src/app/components/inputfeild.tsx
@@ -6,11 +6,11 @@ type Props={
     id:string,
     value:string,
     type:string,
-
+    name:string,
 
 }
 
-const InputFeild = ({title,OnChange,id,value,type}:Props) => {
+const InputFeild = ({title,name,OnChange,id,value,type}:Props) => {
   return (
     <div className="mb-4">
             <label className="block  text-pink-600 mb-2" htmlFor={id}>
@@ -22,6 +22,7 @@ const InputFeild = ({title,OnChange,id,value,type}:Props) => {
               className="w-full bg-transparent text-white px-3 py-2 border-b rounded-lg focus:outline-none focus:border-pink-300 focus:bg-pink-500"
               value={value}
               onChange={OnChange}
+              name={name}
               required
             />
           </div>
