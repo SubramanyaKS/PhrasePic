0845c5371f690e9e450d774676e6c17c4a42570b
Subramanya K S <subramanyaks2205@gmail.com>
2024-06-09 23:09:43 +0530

    added password validation

--- src/app/utils/validate.ts
+++ src/app/utils/validate.ts
@@ -1,3 +1,6 @@
 export const isValidEmail=(email:string)=>{
     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 }
+export const isValidPassword=(password:string)=>{
+    return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(password);
+}
