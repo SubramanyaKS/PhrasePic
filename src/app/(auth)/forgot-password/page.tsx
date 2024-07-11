45f25413207e8bf321a9a7f9ffbea534cf73cbc0
Subramanya K S <subramanyaks2205@gmail.com>
2024-07-11 06:43:27 +0530

    moved forgot password to auth folder

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 
-import InputFeild from '../components/inputfeild';
-import Submitbutton from '../components/submitbutton';
-import { useForgot } from '../hooks/useForgot';
+import InputFeild from '../../components/inputfeild';
+import Submitbutton from '../../components/submitbutton';
+import { useForgot } from '../../hooks/useForgot';
 
 const page = () => {
   const {email,handleSubmit,message,setEmail} = useForgot();
 
   return (
-    <div  className="w-full pink-shadow max-w-md border border-pink-600 p-8 mt-16 rounded-lg shadow-2xl">
+    <div>
       <h2 className="text-2xl text-white font-bold mb-6 text-center" >Forgot Password</h2>
       <form onSubmit={handleSubmit}>
       <InputFeild name="email" value={email} title="Email"  OnChange={(e) => setEmail((e.target as HTMLInputElement).value)} id="email" type="email"/>
