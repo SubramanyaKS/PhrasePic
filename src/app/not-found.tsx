221a71fed9f49b8f1ad935cdef225d12b2446d28
Subramanya K S <subramanyaks2205@gmail.com>
2024-07-13 19:02:16 +0530

    updated not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ const NotFound = () => {
   return (
     <div  className="flex justify-center flex-col items-center text-center text-white">
         <div className='m-12 mb-44'>
-          <h2 className=' m-4 text-9xl font-bold bg-gradient-to-r from-pink-900 via-pink-500 to-indigo-500 text-transparent bg-clip-text '>{constants.errorcode}</h2>
+        <h2 className='drop-shadow m-4 text-9xl font-bold bg-gradient-to-r from-pink-900 via-pink-500 to-indigo-500 text-transparent bg-clip-text '>{constants.errorcode}</h2>
           <h2 className='text-pink-500 text-5xl m-4' >{constants.errorheadline}</h2>
           <p className="text-xl m-4">{constants.errormessage}</p>
           <Button  OnClick={()=>router.replace('/')} title="Home"/>
