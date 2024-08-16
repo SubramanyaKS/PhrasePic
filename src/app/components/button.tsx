b39a270aa36ec644869c05f1884e548337d9e28b
Subramanya K S <subramanyaks2205@gmail.com>
2024-08-16 12:50:09 +0530

    seperated props into different file

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
+import { ButtonProps } from '../utils/types'
 
-interface ButtonProps {
-    title:string;
-    OnClick:() => void;
-}
 
 const Button:React.FC<ButtonProps> = ({title,OnClick}) => {
   return (
     // "generate-button py-4 px-6 mt-4 rounded-full text-white bg-gradient-to-l from-pink-500 to-blue-700 hover:from-purple-500 hover:to-blue-500
-    <button id="btn" className="bg-white btn hover:tracking-widest text-pink-600 font-semibold py-2 px-6 rounded-full shadow-md hover:bg-pink-600 hover:text-white transition duration-300 " onClick={OnClick}>
+    <button id="btn" className="bg-white btn hover:tracking-widest text-pink-600 font-semibold py-2 px-6 rounded-full shadow-2xl hover:bg-pink-600 hover:text-white transition duration-300 " onClick={OnClick}>
         {title}
     </button>
   )
