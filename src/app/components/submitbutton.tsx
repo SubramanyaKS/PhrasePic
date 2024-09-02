f121601854d74ee24257633dfb5f72ace9a9906f
Subramanya K S <subramanyaks2205@gmail.com>
2024-09-02 22:27:23 +0530

    props changed (common props used)

--- src/app/components/submitbutton.tsx
+++ src/app/components/submitbutton.tsx
@@ -1,14 +1,14 @@
 import { PaperAirplaneIcon } from '@heroicons/react/24/outline'
 import React from 'react'
-import { SubmitProps } from '../utils/types'
+import { ButtonProps } from '../utils/types'
 
 
-const Submitbutton = ({title}:SubmitProps) => {
+const Submitbutton = ({title}:ButtonProps) => {
   return (
     <div className='flex justify-center'>
       <button
         type="submit"
-        className="w-full px-5 py-2.5 text-center inline-flex items-center justify-center bg-white hover:tracking-widest font-semibold text-pink-600 py-2 rounded-3xl hover:text-white hover:bg-pink-400 transition duration-300"
+        className="w-full px-5 text-center inline-flex items-center justify-center bg-white hover:tracking-widest font-semibold text-pink-600 py-2 rounded-3xl hover:text-white hover:bg-pink-400 transition duration-300"
       >
         {title} <PaperAirplaneIcon className="size-6 text-pink-600" />
       </button>
